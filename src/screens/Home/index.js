fa58aaa4666fb8ead3adbfe2b28819bfe9be7a6f
victor <victor.magalhaesp@gmail.com>
2020-04-28 15:42:16 -0300

    Implementando header da home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import {Text} from 'react-native';
 
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+import {Wrapper, Header, BalanceContainer, BalanceTitle, Balance} from './styles';
+
 export default function Home() {
-  return <Text>Home</Text>;
+  return (
+    <Wrapper>
+      <Header>
+        <MaterialCommunityIcons name="qrcode-scan" size={30} color="#10c86e" />
+
+        <BalanceContainer>
+          <BalanceTitle>Meu saldo</BalanceTitle>
+          <Balance>R$ 0,00</Balance>
+        </BalanceContainer>
+
+        <AntDesign name="gift" size={30} color="#10c86e" />
+      </Header>
+    </Wrapper>
+  );
 }
